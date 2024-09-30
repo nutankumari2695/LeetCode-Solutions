00d05b04e3369b177cb1ad58144ace7649514e8e
Nutan Singh <120700857+nutankumari2695@users.noreply.github.com>
2024-09-30 16:29:03 +0530

    Time: 2 ms (48.09%), Space: 42.4 MB (34.40%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,6 +1,7 @@
 class Solution {
     public String addBinary(String a, String b) {
          StringBuilder result = new StringBuilder();
+         
         
         int i = a.length() - 1;
         int j = b.length() - 1;
